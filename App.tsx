705e898cde057418156f7dabafb25c4193093a30
DeveloperYamin <developer.yamin@gmail.com>
2022-06-04 20:50:43 +0600

    Connect a user

--- App.tsx
+++ App.tsx
@@ -5,6 +5,8 @@ import { StreamChat } from "stream-chat";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 const API_KEY = "je8mzyd5zvdm";
 const client = StreamChat.getInstance(API_KEY);
@@ -12,6 +14,25 @@ const client = StreamChat.getInstance(API_KEY);
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const connectUser = async () => {
+    try {
+      await client.connectUser(
+        {
+          id: "user-1",
+          name: "User 1",
+          image: "https://i.pravatar.cc/300?img=1",
+        },
+        client.devToken("user-1")
+      );
+    } catch (err: any) {
+      Alert.alert("Error", err.message);
+    }
+  };
+
+  useEffect(() => {
+    connectUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
