283c6a842f644ffbfa04ba3dfd96f449c34a331c
DeveloperYamin <developer.yamin@gmail.com>
2022-06-04 20:22:19 +0600

    Initialize a Stream clientl

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { StreamChat } from "stream-chat";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+
+const API_KEY = "je8mzyd5zvdm";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
